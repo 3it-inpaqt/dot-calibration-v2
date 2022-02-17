60dd8bf7aa96780ebaf69a706a35dbc804991ec4
Victor Yon <yonvictorfr@gmail.com>
2022-02-17 15:03:28 -0500

    Change slurm script to fix the geos dependency issue

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 #SBATCH --account=rrg-rgmelko-ab
-#SBATCH --gres=gpu:p100:1
+#SBATCH --gres=gpu:v100:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4G
 #SBATCH --time=02:00:00
 #SBATCH --job-name=dot-calibration-v2-runs-planner
-module load geos python/3.8.2
+module load geos/3.9.1 python/3.8.10
 
 # Create python virtual env
 virtualenv --no-download "$SLURM_TMPDIR"/venv
 source "$SLURM_TMPDIR"/venv/bin/activate
 pip install --no-index --upgrade pip
 pip install -r requirements.txt
+pip install https://github.com/ufoym/imbalanced-dataset-sampler/archive/master.zip
 
 # Start job
-python runs/run_task_planner.py
+python start_tasks_planner.py
