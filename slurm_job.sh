04d246a04a1ad60e70a5497479ed48aeee963da9
Victor Yon <yonvictorfr@gmail.com>
2022-02-18 12:17:28 -0500

    Modify slurm script to work with pre-installed virtual env

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 #SBATCH --account=rrg-rgmelko-ab
 #SBATCH --gres=gpu:v100:1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=4G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
 #SBATCH --time=02:00:00
 #SBATCH --job-name=dot-calibration-v2-runs-planner
+#SBATCH --output=out/slurm-%j.out
 module load geos/3.9.1 python/3.8.10
 
-# Create python virtual env
-virtualenv --no-download "$SLURM_TMPDIR"/venv
-source "$SLURM_TMPDIR"/venv/bin/activate
-pip install --no-index --upgrade pip
-pip install -r requirements.txt
-pip install https://github.com/ufoym/imbalanced-dataset-sampler/archive/master.zip
+# Create python virtual env (all package have to be available on the system)
+#virtualenv --no-download "$SLURM_TMPDIR"/venv
+# source "$SLURM_TMPDIR"/venv/bin/activate
+#pip install --no-index --upgrade pip
+#pip install -r requirements.txt
+#pip install https://github.com/ufoym/imbalanced-dataset-sampler/archive/master.zip
+
+# If the virtual env is already on the working directory
+source venv/bin/activate
 
 # Start job
 python start_tasks_planner.py
