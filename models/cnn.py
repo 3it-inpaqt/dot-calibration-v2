Fix max pooling logic and add batch normalisation

models/cnn.py
@@ -27,14 +27,28 @@ class CNN(ClassifierNN):
 
         self.conv_layers = nn.ModuleList()
         last_nb_channel = 1
-        for channel, kernel, max_pool in zip(settings.conv_layers_channel,
-                                             settings.conv_layers_kernel,
-                                             settings.max_pooling_layers):
-            self.conv_layers.append(nn.Conv2d(in_channels=last_nb_channel, out_channels=channel,
-                                              kernel_size=(kernel, kernel)))
-            if max_pool:
-                self.conv_layers.append(nn.MaxPool2d(kernel_size=(2, 2)))
 
+        # Create convolution layers
+        for channel, kernel, max_pool, batch_norm in zip(settings.conv_layers_channel,
+                                                         settings.conv_layers_kernel,
+                                                         settings.max_pooling_layers,
+                                                         settings.batch_norm_layers[:len(settings.conv_layers_kernel)]):
+            layer = nn.Sequential()
+            # Convolution
+            layer.append(nn.Conv2d(in_channels=last_nb_channel, out_channels=channel, kernel_size=(kernel, kernel)))
+            # Batch normalisation
+            if batch_norm:
+                layer.append(nn.BatchNorm2d(num_features=channel))
+            # Activation function
+            layer.append(nn.ReLU())
+            # Max pooling
+            if max_pool:
+                layer.append(nn.MaxPool2d(kernel_size=(2, 2)))
+            # Dropout
+            if settings.dropout > 0:
+                layer.append(nn.Dropout(settings.dropout))
+
+            self.conv_layers.append(layer)
             last_nb_channel = channel
 
         # Number of neurons per layer
@@ -46,10 +60,19 @@ class CNN(ClassifierNN):
         # Create fully connected linear layers
         self.fc_layers = nn.ModuleList()
         for i in range(len(fc_layer_sizes) - 1):
-            self.fc_layers.append(nn.Linear(fc_layer_sizes[i], fc_layer_sizes[i + 1]))
+            layer = nn.Sequential()
+            # Fully connected
+            layer.append(nn.Linear(fc_layer_sizes[i], fc_layer_sizes[i + 1]))
+            # Batch normalisation
+            if settings.batch_norm_layers[len(settings.conv_layers_channel) + i - 1]:
+                layer.append(nn.BatchNorm1d(fc_layer_sizes[i + 1]))
+            # Activation function
+            layer.append(nn.ReLU())
+            # Dropout
+            if settings.dropout > 0:
+                layer.append(nn.Dropout(settings.dropout))
 
-        # Create a dropout layer if p > 0
-        self.dropout = nn.Dropout(settings.dropout) if settings.dropout > 0 else None
+            self.fc_layers.append(layer)
 
         # Binary Cross Entropy including sigmoid layer
         self._criterion = nn.BCEWithLogitsLoss()
@@ -67,18 +90,14 @@ class CNN(ClassifierNN):
 
         # Run convolution layers
         for conv in self.conv_layers:
-            x = torch.relu(conv(x))
-            if self.dropout:
-                x = self.dropout(x)
+            x = conv(x)
 
         # Flatten the data (but not the batch)
         x = torch.flatten(x, 1)
 
         # Run fully connected layers
         for fc in self.fc_layers[:-1]:
-            x = torch.relu(fc(x))
-            if self.dropout:
-                x = self.dropout(x)
+            x = fc(x)
 
         # Last layer doesn't use sigmoid because it's include in the loss function
         x = self.fc_layers[-1](x)

utils/misc.py
@@ -43,7 +43,7 @@ def clip(n, smallest, largest):
     return max(smallest, min(n, largest))
 
 
-def calc_out_conv_layers(input_size: Tuple[int, int], layers: Iterable[Union[nn.Conv2d, nn.MaxPool2d]]) \
+def calc_out_conv_layers(input_size: Tuple[int, int], layers: Iterable[Union[nn.Conv2d, nn.MaxPool2d, nn.Sequential]]) \
         -> Tuple[int, ...]:
     """
     Compute the size of output dimension of a list of convolutional and max pooling layers, according to the initial
@@ -54,8 +54,19 @@ def calc_out_conv_layers(input_size: Tuple[int, int], layers: Iterable[Union[nn.
     :param layers: The list of convolutional layers.
     :return: The final output dimension.
     """
-    out_h, out_w = input_size
+    # Keep only the layer that affect the size of the data
+    resize_layers = []
     for la in layers:
+        if type(la) in [nn.Conv2d, nn.MaxPool2d]:
+            resize_layers.append(la)
+        elif type(la) is nn.Sequential:
+            for sub_la in la:
+                # TODO make it recursive
+                if type(sub_la) in [nn.Conv2d, nn.MaxPool2d]:
+                    resize_layers.append(sub_la)
+
+    out_h, out_w = input_size
+    for la in resize_layers:
         # Compatibility with Max Pooling and BayesianConv2d where properties are integer instead of tuple
         padding = (la.padding, la.padding) if isinstance(la.padding, int) else la.padding
         dilation = (la.dilation, la.dilation) if isinstance(la.dilation, int) else la.dilation
@@ -65,7 +76,7 @@ def calc_out_conv_layers(input_size: Tuple[int, int], layers: Iterable[Union[nn.
         out_h = (out_h + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) / stride[0] + 1
         out_w = (out_w + 2 * padding[1] - dilation[1] * (kernel_size[1] - 1) - 1) / stride[1] + 1
 
-    return layers[-1].out_channels, int(out_h), int(out_w)
+    return resize_layers[-1].out_channels, int(out_h), int(out_w)
 
 
 def yaml_preprocess(item: Any) -> Union[str, int, float, List, Dict]: