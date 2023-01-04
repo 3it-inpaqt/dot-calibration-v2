b6466ef3964fdc7003ea7a29665bad08c13f6fd0
Victor Yon <yonvictorfr@gmail.com>
2023-01-04 17:49:55 -0500

    Create a script to run everything at once

---
+++ start_full_exp.py
@@ -0,0 +1,30 @@
+from runs.planner_presets import full_scan_all, full_scan_cross_valid, run_tasks_planner, tune_all, \
+    tune_all_cross_valid, tune_oracle, uncertainty_study_all, uncertainty_study_cross_valid
+
+# Start a batch of runs with pre-defined settings to get every result for a specific seed.
+# It is possible to run multiple instance of this script in parallel with different seeds.
+if __name__ == '__main__':
+    # Run tuning with oracle and random as a baseline
+    run_tasks_planner(tune_oracle, skip_existing_runs=True, tuning_task=True)
+
+    # =================== Mixed Diagrams ===================
+
+    # Training and tuning
+    run_tasks_planner(tune_all, skip_existing_runs=True, tuning_task=True)
+
+    # Full scan for each diagram for the first seed
+    run_tasks_planner(full_scan_all, skip_existing_runs=True, tuning_task=True)
+
+    # Uncertainty study
+    run_tasks_planner(uncertainty_study_all, skip_existing_runs=True, tuning_task=False)
+
+    # ================== Cross-Validation ==================
+
+    # Training and tuning
+    run_tasks_planner(tune_all_cross_valid, skip_existing_runs=True, tuning_task=True)
+
+    # Full scan for each diagram for the first seed
+    run_tasks_planner(full_scan_cross_valid, skip_existing_runs=True, tuning_task=True)
+
+    # Uncertainty study
+    run_tasks_planner(uncertainty_study_cross_valid, skip_existing_runs=True, tuning_task=False)
