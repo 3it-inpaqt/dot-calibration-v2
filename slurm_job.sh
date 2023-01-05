80dc003abe3ed3b0eb6cc77443508af12cf692d7
Victor Yon <yonvictorfr@gmail.com>
2023-01-05 12:15:03 -0500

    Add full exp run config and update requirements version

--- slurm_job.sh
+++ slurm_job.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:v100:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --time=10:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=dot-calibration-v2-runs-planner
 #SBATCH --output=out/slurm-%j.out
 module load geos/3.9.1 python/3.8.10
@@ -21,4 +21,4 @@ module load geos/3.9.1 python/3.8.10
 source venv/bin/activate
 
 # Start job
-python start_tasks_planner.py
+python start_full_exp.py
