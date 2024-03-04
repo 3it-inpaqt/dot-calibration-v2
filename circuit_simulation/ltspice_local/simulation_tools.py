e80c73ea39b02d89aa5bd1ecfbdedcc83a07711f
HusseinA-1Qbit <129790314+HusseinA-1Qbit@users.noreply.github.com>
2024-03-04 17:17:17 -0500

    Update simulation_tools.py

--- circuit_simulation/ltspice_local/simulation_tools.py
+++ circuit_simulation/ltspice_local/simulation_tools.py
@@ -69,7 +69,7 @@ def simulate(spice_exe_path, file_path):
         if psutil.WINDOWS:
            runcmd = '"' + spice_exe_path + '" -netlist "' + file_path + '.' + config.LTSpice_asc_filetype + '"'
         elif psutil.LINUX:
-            runcmd = '' + spice_exe_path + ' -netlist "' + file_path + '.' + config.LTSpice_asc_filetype + ''
+            runcmd = '' + spice_exe_path + ' -netlist ' + file_path + '.' + config.LTSpice_asc_filetype + ''
         call(runcmd)
         while os.path.exists(file_path + '.net') == False:
             print('Waiting for "{file_path}" + .net to be created')
